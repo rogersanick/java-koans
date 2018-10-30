d32761c293a0e9ffc6d0d9b42ede46eb29bc2fed
rogersanick <nicholasrogers@MacBook-Dev-09.local>
2018-10-30 14:39:46 -0400

    Finished all Java Koans

--- koans/src/intermediate/AboutSerialization.java
+++ koans/src/intermediate/AboutSerialization.java
@@ -8,7 +8,6 @@ import java.util.logging.Logger;
 import static com.sandwich.koan.constant.KoanConstants.__;
 import static com.sandwich.util.Assert.assertEquals;
 
-
 public class AboutSerialization {
 
     @Koan
@@ -26,7 +25,7 @@ public class AboutSerialization {
         try {
             is = new ObjectInputStream(new FileInputStream("SerializeFile"));
             String otherString = (String) is.readObject();
-            assertEquals(otherString, __);
+            assertEquals(otherString, "Hello world");
         } finally {
             closeStream(is);
         }
@@ -55,7 +54,7 @@ public class AboutSerialization {
         try {
             is = new ObjectInputStream(new FileInputStream("SerializeFile"));
             Starship onTheOtherSide = (Starship) is.readObject();
-            assertEquals(onTheOtherSide.maxWarpSpeed, __);
+            assertEquals(onTheOtherSide.maxWarpSpeed, 9);
         } finally {
             closeStream(is);
         }
@@ -104,7 +103,7 @@ public class AboutSerialization {
         try {
             is = new ObjectInputStream(new FileInputStream("SerializeFile"));
             Car deserializedCar = (Car) is.readObject();
-            assertEquals(deserializedCar.engine.type, __);
+            assertEquals(deserializedCar.engine.type, "diesel");
         } finally {
             closeStream(is);
         }
@@ -129,7 +128,7 @@ public class AboutSerialization {
             marker += "Exception";
         }
         os.close();
-        assertEquals(marker, __);
+        assertEquals(marker, "Start Exception");
     }
 
     @SuppressWarnings("serial")
@@ -161,7 +160,7 @@ public class AboutSerialization {
         try {
             is = new ObjectInputStream(new FileInputStream("SerializeFile"));
             Dog otherDog = (Dog) is.readObject();
-            assertEquals(otherDog.name, __);
+            assertEquals(otherDog.name, "snoopy");
         } finally {
             closeStream(is);
         }
@@ -199,7 +198,7 @@ public class AboutSerialization {
             is = new ObjectInputStream(new FileInputStream("SerializeFile"));
             MilitaryPlane otherPlane = (MilitaryPlane) is.readObject();
             // Does this surprise you?
-            assertEquals(otherPlane.name, __);
+            assertEquals(otherPlane.name, null);
 
             // Think about how serialization creates objects...
             // It does not use constructors! But if a parent object is not serializable
